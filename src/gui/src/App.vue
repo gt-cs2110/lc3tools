921c000f1196d62f70cebef31573205191fb6e17
Henry Bui <henry.h.bui05@gmail.com>
2024-07-27 22:41:31 +0200

    create base electron forge template (with vue + lc3-backend)

---
+++ src/gui/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+  </template>
+  
+<script setup lang="ts">
+  // Declare lc3's presence.
+  import type LC3Backend from "lc3-backend";
+  declare var lc3: typeof LC3Backend;
+
+  console.log('👋 This message is being logged by "App.vue", included via Vite');
+  console.log(lc3.addInput);
+</script>
+
+<style lang="css">
+  @import "index.css";
+</style>
