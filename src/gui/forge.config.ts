0c6c72d6cd849bfb36435867eedde4c2f1d3f252
Henry Bui <henry.h.bui05@gmail.com>
2024-08-23 16:48:42 -0400

    attempt github ci

--- src/gui/forge.config.ts
+++ src/gui/forge.config.ts
@@ -3,6 +3,7 @@ import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerDMG } from "@electron-forge/maker-dmg";
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
+import { PublisherGithub } from '@electron-forge/publisher-github';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
@@ -14,6 +15,14 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerDMG(), new MakerRpm({}), new MakerDeb({})],
+  publishers: [new PublisherGithub({
+    repository: {
+      owner: "endorpersand",
+      name: "lc3tools"
+    },
+    prerelease: true,
+    draft: true
+  })],
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
