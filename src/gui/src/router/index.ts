40f4d01c9630ba551dd9ff44e3087b837b689c2d
Henry Bui <henry.h.bui05@gmail.com>
2024-10-01 11:59:50 -0400

    use web history over mem history

--- src/gui/src/router/index.ts
+++ src/gui/src/router/index.ts
@@ -1,9 +1,9 @@
 import Editor from "../components/editor/Editor.vue";
 import Simulator from "../components/simulator/Simulator.vue";
-import { createMemoryHistory, createRouter } from "vue-router"
+import { createWebHistory, createRouter } from "vue-router";
 
 export default createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: '/editor',
